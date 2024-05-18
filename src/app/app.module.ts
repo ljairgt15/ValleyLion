cdef1834db43b530da1f4df6374a8ab409aab31c
JG <ludwing.gomez@epn.edu.ec>
2024-05-18 16:07:41 -0500

    feat: new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { OnlineStoreComponent } from './components/online-store/online-store.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TrophiesComponent } from './components/trophies/trophies.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { NewsComponent } from './components/news/news.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutUsComponent,
+    OnlineStoreComponent,
+    ContactComponent,
+    GalleryComponent,
+    TrophiesComponent,
+    TeamsComponent,
+    NewsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/online-store/online-store.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OnlineStoreComponent } from './online-store.component';
+
+describe('OnlineStoreComponent', () => {
+  let component: OnlineStoreComponent;
+  let fixture: ComponentFixture<OnlineStoreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OnlineStoreComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OnlineStoreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
