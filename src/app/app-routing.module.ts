7551a1b320824307ebf5134838617b0c6db96f94
JG <ludwing.gomez@epn.edu.ec>
2024-05-24 10:57:50 -0500

    feat: routing MODELS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { OnlineStoreComponent } from './components/online-store/online-store.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { TrophiesComponent } from './components/trophies/trophies.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NewsComponent } from './components/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'**',component:IndexComponent},
+  {path:'tienda',component:OnlineStoreComponent},
+  {path:'nosotros',component:AboutUsComponent},
+  {path:'contactanos',component:ContactComponent},
+  {path:'equipos', component:TeamsComponent},
+  {path:'trofeos', component: TrophiesComponent},
+  {path:'galeria', component:GalleryComponent},
+  {path:'noticias',component:NewsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
