3cf65483d3ee0374c1d38843a8f7d05baf6c81ab
JG <ludwing.gomez@epn.edu.ec>
2024-05-24 22:47:04 -0500

    feat: news card

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { INews } from 'src/app/models/news';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+  @Input() numNews: number = 3;
 
   constructor() { }
+  newsList: INews[] = [
+    {
+      title: 'Independiente del Valle inauguró el primer estadio de Latinoamérica para un equipo femenino: Dragonas IDV',
+      description: 'Dragonas',
+      imageUrl: 'assets/logo.png',
+      date: 'mayo 13, 2024'
+    },
+    {
+      title: 'COMUNICADO DE PRENSA: Juan Tuquerrez (13) al Club Deportivo El Nacional',
+      description: 'Formativas',
+      imageUrl: 'assets/news/juanitoNacho.jpg',
+      date: 'mayo 13, 2024'
+    },
+    {
+      title: 'Independiente del Valle inauguró el primer estadio de Latinoamérica para un equipo femenino: Dragonas IDV',
+      description: 'Dragonas',
+      imageUrl: 'assets/index/equipo.jpg',
+      date: 'mayo 13, 2024'
+    },
+    {
+      title: 'COMUNICADO DE PRENSA: Juan Tuquerrez (13) al Club Deportivo El Nacional',
+      description: 'Formativas',
+      imageUrl: 'assets/index/equipo2.jpg',
+      date: 'mayo 13, 2024'
+    },
+  ];
+
+  displayedNews: INews[] = [];
+
 
   ngOnInit(): void {
+    this.displayedNews = this.newsList.slice(0, this.numNews);
   }
 
 }
---
+++ src/app/models/news.ts
@@ -0,0 +1,6 @@
+export interface INews {
+    title: string;
+    description: string;
+    imageUrl: string;
+    date: string;
+  }
