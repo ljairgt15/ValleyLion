5b31aceffde74a9c5d3e51be0b7e3e7a0a779c8d
JG <ludwing.gomez@epn.edu.ec>
2024-05-29 21:16:02 -0500

    feat: contact us

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConstants } from 'src/constants/AppConstants';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  whatsappUrl: string = AppConstants.WHATSAPP_URL;
+  facebookUrl: string = AppConstants.FACEBOOK_URL;
+  instagramUrl: string = AppConstants.INSTAGRAM_URL;
+  tiktokUrl: string = AppConstants.TIKTOK_URL;
+  cellPhoneNumber: string = "0"+AppConstants.CELLPHONE_NUMBER;
+  academyName:string = AppConstants.ACADEMY_NAME;
+
 
   constructor() { }
 
