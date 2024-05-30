ead40d0132ca7bb20245f2e8c6afd58b629dda10
JG <ludwing.gomez@epn.edu.ec>
2024-05-29 21:15:45 -0500

    feat: global variables

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConstants } from 'src/constants/AppConstants';
 
 @Component({
   selector: 'app-nav',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  whatsappUrl: string = AppConstants.WHATSAPP_URL;
+  facebookUrl: string = AppConstants.FACEBOOK_URL;
+  instagramUrl: string = AppConstants.INSTAGRAM_URL;
+  tiktokUrl: string = AppConstants.TIKTOK_URL;
+  cellPhoneNumber: string = "0"+AppConstants.CELLPHONE_NUMBER;
+  academyName:string = AppConstants.ACADEMY_NAME;
 
   constructor() { }
 
---
+++ src/constants/AppConstants.ts
@@ -0,0 +1,10 @@
+export class AppConstants {
+    public static readonly CELLPHONE_NUMBER: string ="988928886";
+    public static readonly WHATSAPP_URL: string = `https://wa.me/${AppConstants.CELLPHONE_NUMBER}`;
+    public static readonly FACEBOOK_URL: string = "https://www.facebook.com/leones.oyambarillo";
+    public static readonly INSTAGRAM_URL: string = "https://www.instagram.com/leones.oyambarillo";
+    public static readonly TIKTOK_URL: string = "https://www.tiktok.com/@leones.oyambarillo";
+    public static readonly ACADEMY_NAME: string = "Academia Formativa de Fútbol Leones del Valle";
+
+
+}
