4774530f61c94ddeee0243eac8942544fabfb76b
JG <ludwing.gomez@epn.edu.ec>
2024-05-29 21:31:32 -0500

    feat: manage form in behiind code

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConstants } from 'src/constants/AppConstants';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -14,8 +15,27 @@ export class ContactComponent implements OnInit {
   cellPhoneNumber: string = "0"+AppConstants.CELLPHONE_NUMBER;
   academyName:string = AppConstants.ACADEMY_NAME;
 
+  contactForma: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.contactForma = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
 
-  constructor() { }
+  onSubmit(): void {
+    if (this.contactForma.valid) {
+      const formData = this.contactForma.value;
+      console.log('Form Data Submitted:', formData);
+      // Aquí puedes agregar la lógica para enviar los datos al servidor, por ejemplo usando HttpClient
+      // this.httpClient.post('your-api-url', formData).subscribe(response => {
+      //   console.log('Response from server:', response);
+      // });
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 
   ngOnInit(): void {
   }
