7a9e695ce65f779728e2466dfb3643ee6f7ebdc0
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2024-08-30 15:55:14 -0500

    feat: component trophies

--- src/app/components/trophies/trophies.component.ts
+++ src/app/components/trophies/trophies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from 'src/app/models/title';
 
 @Component({
   selector: 'app-trophies',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trophies.component.css']
 })
 export class TrophiesComponent implements OnInit {
+  titles: Title[] = [
+    {
+      name: 'Torneo Castro Loaiza',
+      dates: ['11 de diciembre del año 2023'],
+      category: 'U12',
+      imageUrl: 'ruta_a_la_imagen',
+    },
+    {
+      name: 'San Carlos JD PRO Events',
+      timesWon: 1,
+      dates: ['1955-56'],
+      category: 'U11',
+      imageUrl: 'ruta_a_la_imagen',
+    },
+    {
+      name: 'Oyambarillo',
+      timesWon: 1,
+      dates: ['2022'],
+      category: 'U8',
+      imageUrl: 'ruta_a_la_imagen',
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/models/title.ts
@@ -0,0 +1,7 @@
+export interface Title{
+    name: string;
+    timesWon ?: number;
+    dates: string[];
+    category: string;
+    imageUrl: string;
+}
