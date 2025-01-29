35f7d3709dc033c572352a6b8e4404ff8832946a
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2025-01-30 04:01:06 +0800

    feat: change logic news show

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -7,7 +7,7 @@ import { INews } from 'src/app/models/news';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-  @Input() numNews: number = 3;
+  @Input() numNews: number = 0;
 
   constructor() { }
   newsList: INews[] = [
@@ -29,13 +29,34 @@ export class NewsComponent implements OnInit {
       imageUrl: 'assets/news/damianVinotinto.jpg',
       date: 'mayo 13, 2024'
     },
+    {
+      title: 'COMUNICADO DE PRENSA: Damián Gómez (13) al Vinotinto FC',
+      description: 'FORMATIVAS PROFESIONALES',
+      imageUrl: 'assets/news/damianVinotinto.jpg',
+      date: 'mayo 13, 2024'
+    },
+    {
+      title: 'COMUNICADO DE PRENSA: Juan Tuquerrez (13) al Vinotinto FC',
+      description: 'FORMATIVAS PROFESIONALES',
+      imageUrl: 'assets/news/juanitoNacho.jpg',
+      date: 'mayo 13, 2024'
+    },
+    {
+      title: 'COMUNICADO DE PRENSA: Damián Gómez (13) al Vinotinto FC',
+      description: 'FORMATIVAS PROFESIONALES',
+      imageUrl: 'assets/news/damianVinotinto.jpg',
+      date: 'mayo 13, 2024'
+    },
   ];
 
   displayedNews: INews[] = [];
 
 
   ngOnInit(): void {
-    this.displayedNews = this.newsList.slice(0, this.numNews);
+    if(this.numNews!=0)
+      this.displayedNews = this.newsList.slice(0, this.numNews);
+    else
+      this.displayedNews = this.newsList
   }
 
 }
