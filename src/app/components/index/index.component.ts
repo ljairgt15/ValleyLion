9100425e3432ce754bbef3fd958cc783ff55d069
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2025-01-29 10:38:45 -0500

    feat: fris t image cover then videop

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -8,41 +8,23 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2, HostListener } fro
 export class IndexComponent implements OnInit {
 
   @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
-  private videoReproducido = false; // Evita múltiples intentos de reproducción
+  mostrarPortada: boolean = true;
+  private videoReproducido = false;
 
   constructor(private renderer: Renderer2) { }
+
   ngOnInit(): void {
-    // Escuchar evento de scroll
-    this.renderer.listen('window', 'scroll', () => this.reproducirVideo());
-    this.renderer.listen('window', 'click', () => this.reproducirVideo());
-
   }
-/*
 
-   ngOnInit(): void {
-  const boton = document.createElement('button');
-  boton.style.position = 'absolute';
-  boton.style.opacity = '0';
-  boton.style.pointerEvents = 'none';
-  document.body.appendChild(boton);
-
-  boton.click(); // Simula una interacción del usuario
-
-  setTimeout(() => {
-    this.reproducirVideo();
-    document.body.removeChild(boton); // Elimina el botón después de la interacción
-  }, 500);
-}
-    */
-
- private reproducirVideo(): void {
-    if (this.videoPlayer && this.videoPlayer.nativeElement && !this.videoReproducido) {
+  registrarInteraccion(event: MouseEvent): void {
+    if (!this.videoReproducido && this.videoPlayer?.nativeElement) {
       this.videoPlayer.nativeElement.play().then(() => {
-        this.videoReproducido = true; // Solo reproducir una vez
+        this.videoReproducido = true;
+        this.mostrarPortada = false;
+        console.log('✅ Video iniciado después del clic.');
       }).catch(error => {
-        console.error('Error al reproducir el video:', error);
+        console.error('Error al intentar reproducir el video:', error);
       });
     }
   }
-
 }
